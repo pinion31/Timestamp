7978ac2d0032839677f3279d2d56d84bc7cfc8d7
Chris Cantu <pinion31@gmail.com>
2017-06-25 17:23:09 -0500

    code complete

--- server.js
+++ server.js
@@ -1,12 +1,64 @@
-var express = require('express');
-const bodyparser = require('body-parser');
+var http = require('http');
+var url = require('url');
+var fs = require('fs');
+var queryString = require('query-string');
 
-const app = express();
-app.use(express.static('static'));
-app.use(bodyparser.json());
+http.createServer(function(req,res) {
+  var q = url.parse(req.url, true);
+  fs.readFile('./static/index.html', function (err,data) {
+    if (err){
+      return err;
+    }
+    var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October',
+    'November', 'December'];
 
-app.get('/', (req,res) => {
-  res.json(req.body);
-}).listen(3000, () => {
-  console.log('App started on port 3000');
-});
+    if (q.search) {
+      var parsed = queryString.parse(q.search);
+      res.writeHead(200, {'Content-type': 'text/html'});
+
+      if (Number.isInteger(+parsed.request)) {
+
+        var newDate = new Date(+parsed.request * 1000);
+        res.write("Natural Time: " + (months[newDate.getMonth()] + " " + newDate.getDate() + ", " + (newDate.getYear() + 1900).toString()) + "<br>");
+        var unixTime = (newDate.getTime()/1000).toString();
+        res.end("Unix Time: " + unixTime);
+      }
+      else {
+        var dateRaw = parsed.request.split(" ");
+
+        if (dateRaw.length === 3) {
+          var month = dateRaw[0].slice(0,3); //takes first 3 letters of month
+          var day = dateRaw[1].slice(0,2); //day of month
+          var year = dateRaw[2]; //year
+
+          var newDate = new Date(month + " " + day + " " + year);
+
+          if (Number.isInteger(newDate.getDate())) {
+            res.write("Natural Time: " + (months[newDate.getMonth()] + " " + newDate.getDate() + ", " + (newDate.getYear() + 1900).toString()) + "<br>");
+            var unixTime = (newDate.getTime()/1000).toString();
+            res.end("Unix Time: " + unixTime);
+          }
+          else {
+            res.end("Invalid Date");
+          }
+        }
+        else {
+          res.end("Invalid Date");
+        }
+
+      }
+
+
+
+
+    }
+
+    else {
+      res.writeHead(200, {'Content-type': 'text/html'});
+      res.write(data);
+
+      return res.end();
+    }
+  });
+
+}).listen(3000);
